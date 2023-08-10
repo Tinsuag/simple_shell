f5dc0a437a651ded1c432f3082ec035f8abb3531
Tinsuag <tinsaeareg.work@gmail.com>
2023-08-10 23:56:20 +0300

    tried my best today

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int get_line(void);
+char type_prompt(void);
+include shell(char *buf, size_t n);
+#endif 
