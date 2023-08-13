49879fe43d3faec98f1fd3128b950851c8580971
Tinsuag <tinsaeareg.work@gmail.com>
2023-08-13 22:45:52 +0300

    Lines

--- path.c
+++ path.c
@@ -1,38 +1,51 @@
 #include "main.h"
 
-char *path_(char *command)
-{
-char *path, *path_cpy, *path_token;
-char *file_path;
-struct stat buffer;
-int command_length = 0, directory_length;
-path = getenv("PATH");
-if (path){
-path_cpy = strdup(path);
-command_length = strlen(command);
-path_token = strtok(path_cpy, ":");
-directory_length = strlen(path_token);
-file_path = malloc(command_length + directory_length +2);
-strcpy(file_path, path_token);
-strcat(file_path, "/");
-strcat(file_path, command);
-strcat(file_path, "\0");
-if (stat(file_path, &buffer) == 0)
-{
-free(path_cpy);
-return (file_path);
-}
-else
-{
-free (file_path);
-path_token = strtok(NULL, "/");
-}
-free(path_cpy);
-if (stat(command, &buffer) == 0)
-{
-    return (command);
-}
-return (NULL);
-}
-return (NULL);
+char *path_(char *command){
+    char *path, *path_copy, *path_token, *file_path;
+    int command_length, directory_length;
+    struct stat buffer;
+
+    path = getenv("PATH");
+
+    if (path){
+        path_copy = strdup(path);
+        command_length = strlen(command);
+
+        path_token = strtok(path_copy, ":");
+
+        while(path_token != NULL){
+            directory_length = strlen(path_token);
+            file_path = malloc(command_length + directory_length + 2); /* NB: we added 2 for the slash and null character we will introduce in the full command */
+            strcpy(file_path, path_token);
+            strcat(file_path, "/");
+            strcat(file_path, command);
+            strcat(file_path, "\0");
+
+            if (stat(file_path, &buffer) == 0){
+                free(path_copy);
+
+                return (file_path);
+            }
+            else{
+                free(file_path);
+                path_token = strtok(NULL, ":");
+
+            }
+
+        }
+
+        free(path_copy);
+
+        if (stat(command, &buffer) == 0)
+        {
+            return (command);
+        }
+
+
+        return (NULL);
+
+    }
+
+
+    return (NULL);
 }
